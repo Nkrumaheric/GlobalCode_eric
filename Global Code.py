1b9448e2c3a2f74901ffa67558fb912dcf983479
global_eric <ericnkrumah07@gmail.com>
2019-07-10 17:12:55 +0100

    My_First_Project

---
+++ Global Code.py
@@ -0,0 +1,19 @@
+'''def shape():
+    for x in range(4):
+        
+        print("*"*4)
+        
+shape()'''
+
+def box():
+    for y in range(5):
+        print("*"*y)
+    while y > 0:
+        y -=1
+        print("*"*y)
+            
+            
+            
+             
+
+box ()
